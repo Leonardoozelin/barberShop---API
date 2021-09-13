3a3deb568548e615b003531a33d142695470bb2d
jrtisistemas <jrtisistemas@github.com>
2021-09-13 08:47:07 -0300

    Update Session Controlle

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -5,24 +5,24 @@ import User from '../models/User';
 import File from '../models/File';
 import authConfig from '../../config/auth';
 
-class SessionController{
-    async store(req, res){
+class SessionController {
+    async store(req, res) {
         const schema = Yup.object().shape({
             email: Yup.string().email().required(),
             password: Yup.string().required(),
         });
 
-        if (!(await schema.isValid(req.body))){
+        if (!(await schema.isValid(req.body))) {
             return res.status(400).json({ error: 'Validation Fails' });
         }
 
         const { email, password } = req.body;
 
         const user = await User.findOne({
-            where: {email},
+            where: { email },
             include: [
                 {
-                    model:File,
+                    model: File,
                     as: 'avatar',
                     attributes: ['id', 'path', 'url'],
                 }
@@ -30,10 +30,10 @@ class SessionController{
         });
 
         if (!user) {
-            return res.status(401).json({ error: 'User not found'});
+            return res.status(401).json({ error: 'User not found' });
         }
 
-        if (!(await user.checkPassword(password))){
+        if (!(await user.checkPassword(password))) {
             return res.status(401).json({ error: 'Password does not match' });
         }
 
